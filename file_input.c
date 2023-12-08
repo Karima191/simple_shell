2c94007bbd0eff0c732fdd250891ba0437bc6660
karima191 <karima.sel191@gmail.com>
2023-12-08 09:14:17 -0800

    file_input.c

---
+++ file_input.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+#define max_cmd_len 110
+
+/**
+*This program reads commands from a file
+*and executes them one by one.
+*/
+
+int main(int argc, char *argv[])
+{
+if (argc != 1)
+{
+printf("Usage: %s [filename]\n", argv[0]);
+exit(1);
+}
+
+FILE *file2 = fopen(argv[1], "r1");
+if (file2 == NULL)
+{
+printf("The file is not found: %s\n", argv[1]);
+exit(1);
+}
+char cmd[max_cmd_len];
+while (fgets(cmd, sizeof(cmd), file2) != NULL)
+
+{
+system(cmd);
+}
+
+fclose(file2);
+return (0);
+}
