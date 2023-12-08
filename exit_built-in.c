55f039b1df99f0b8747820f538af2e18035b6daa
Meriem316 <meriemsel316@gmail.com>
2023-12-08 12:17:40 -0800

    exit_built-in.c

---
+++ exit_built-in.c
@@ -0,0 +1,120 @@
+[21:44, 06/12/2023] Meriem: #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define max_input_size 1000
+#define max_path_size 1000
+#define wait
+
+/**
+* Function to execute a command
+* This function takes a command as input
+*and executes it using fork and execlp.
+* It handles the cases where the fork fails or the command is not found.
+*/
+
+void execCmd(char *cmd)
+{
+pid_t pid = fork();
+if (pid < 0)
+{
+perror("Fork is failed");
+exit(EXIT_FAILURE);
+}
+else if (pid == 0)
+{
+execlp(cmd, cmd, (char *)NULL);
+fprintf(stderr, "The command is not found:\n%s\n", cmd);
+exit(EXIT_FAILURE);
+}
+else
+{
+wait(NULL);
+}
+}
+/**
+* Main function
+* This function reads user input, tok…
+[21:44, 06/12/2023] Meriem: Task 3
+[21:44, 06/12/2023] Meriem: Task name path.c
+[21:44, 06/12/2023] Meriem: Task 4 name of file is exit_built-in.c
+[21:44, 06/12/2023] Meriem: #include "shell.h"
+
+#define max_input_size 1010
+#define max_path_size 1010
+#define wait
+
+/**
+* Function to execute a command
+* This function takes a command as input
+*and executes it using fork and execlp.
+* It handles the cases where the fork fails or the command is not found.
+**/
+void execCmd(char *cmd)
+{
+pid_t pid = fork();
+if (pid < 0)
+{
+perror("Error fork is failed");
+exit(EXIT_FAILURE);
+}
+else if (pid == 0)
+{
+execlp(cmd, cmd, (char *)NULL);
+fprintf(stderr, "This command is not found:\n%s\n", cmd);
+exit(EXIT_FAILURE);
+}
+else
+{
+wait(NULL);
+}
+/**
+* Main function
+* This function reads user input, tokenizes it,
+* checks for the command in the system's PATH,
+*and executes the command if it exists.
+* The function also checks if the user inputs
+* the 'exit' command, in which case it terminates the shell.
+**/
+int main(void)
+{
+char input[max_input_size];
+char *path = getenv("PATH");
+int exitFlag = 0;
+while (!exitFlag)
+{
+printf("$\t");
+if (fgets(input, max_input_size, stdin) == NULL)
+{
+break;
+}
+
+input[strcspn(input, "\n")] = '\0';
+if (strcmp(input, "exit") == 0)
+{
+exitFlag = 1;
+printf("Exiting the shell!...\n");
+}
+else
+{
+char *token = strtok(input, " ");
+if (token == NULL)
+{
+continue;
+}
+char cmdPath[max_path_size];
+char *pathToken = strtok(path, ":");
+
+while (pathToken != NULL)
+{
+snprintf(cmdPath, sizeof(cmdPath), "%s/%s", pathToken, token);
+if (access(cmdPath, W_OK) == 0)
+{
+execCmd(cmdPath);
+break;
+}
+pathToken = strtok(NULL, ":");}
+}
+return (0);
+}
