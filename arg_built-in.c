f540d87f7e45656a9542d885b3b977530ef2b8b3
Meriem316 <meriemsel316@gmail.com>
2023-12-09 02:29:54 -0800

    arg_built-in.c

--- arg_built-in.c
+++ arg_built-in.c
@@ -11,31 +11,33 @@
 int main(void)
 {
 char input[max_input_size];
-fgets(input, max_input_size, stdin);
-
-char *tok = strtok(input, " \n");
 int s = 0;
 
 while (1)
 {
 printf("Shell $  ");
-}
 
+fgets(input, max_input_size, stdin);
+
+char *tok = strtok(input, " \n");
+
+if (tok == NULL)
+{
+	continue;
+}
 if (strcmp(tok, "exit") == 0)
 {
 tok = strtok(NULL, " \n");
-}
-if (tok == NULL)
+if (tok != NULL)
 {
+	s = atoi(tok);
+}
 exit(s);
 }
 else
 {
-s = atoi(tok);
-exit(s);
-}
-}
 printf("the command is not recognized: %s\n", tok);
+}
 }
 
 return (0);
