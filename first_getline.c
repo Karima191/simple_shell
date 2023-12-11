67505b8ac2001f6c4bef3bd58cb5f4ce67af0cac
Meriem316 <meriemsel316@gmail.com>
2023-12-11 00:57:12 -0800

    first_getline.c

--- first_getline.c
+++ first_getline.c
@@ -12,6 +12,8 @@ char *first_getline(void)
 	int buf_idx = 0;
 	int buf_size;
 	char buf[max_input_size];
+	int line_length;
+	char *line;
 if (buf_idx >= buf_size)
 {
 buf_size = read(0, buf, max_input_size);
@@ -22,15 +24,14 @@ return (NULL);
 buf_idx = 0;
 }
 
-int line_length;
 char *nline_ptr = strchr(buf + buf_idx, '\n');
 if (nline_ptr != NULL)
 {
 *nline_ptr = '\0';
 
-int line_length = nline_ptr - (buf + buf_idx);
+line_length = nline_ptr - (buf + buf_idx);
 
-char *line = malloc(line_length + 1);
+line = malloc(line_length + 1);
 strncpy(line, buf + buf_idx, line_length);
 line[line_length] = '\0';
 
